82b0384f748688a8d5780b16c4b897012dcc33fc
kaiz112 <bananahaha01@gmail.com>
2025-07-22 18:41:13 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ APP = ['pptx_tool_gui.py']
 DATA_FILES = []
 OPTIONS = {
     'argv_emulation': True,
-    'iconfile': 'icon.icns',  # Optional: if you have a macOS icon
+    #'iconfile': 'icon.icns',  # Optional: if you have a macOS icon
     'packages': ['pptx', 'pandas', 'openpyxl']
 }
 
